cecf0c8aab06460eecf2686b1e4b21f2b97c82aa
Willian Oliveira <wijloc@gmail.com>
2020-11-25 19:23:40 -0300

    style: editorConfig

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>    
+  <React.StrictMode>
     <App/>
   </React.StrictMode>,
   document.getElementById('root')
