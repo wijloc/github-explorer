82b20566a75eafbef92a42ace9389c13887aa8ca
Willian Oliveira <wijloc@gmail.com>
2020-11-28 15:56:32 -0300

    feat: consume github api to show repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepositories(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([repository, ...repositories]);
+
+    setNewRepo('');
+  }
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepositories}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
-            alt="Willian Oliveira"
-          />
-          <div>
-            <strong>wijloc/gostack</strong>
-            <p>Short description</p>
-          </div>
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
-            alt="Willian Oliveira"
-          />
-          <div>
-            <strong>wijloc/gostack</strong>
-            <p>Short description</p>
-          </div>
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
-            alt="Willian Oliveira"
-          />
-          <div>
-            <strong>wijloc/gostack</strong>
-            <p>Short description</p>
-          </div>
-          <FiChevronRight />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))}
       </Repositories>
     </>
   );
