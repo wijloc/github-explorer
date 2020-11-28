1c3b107ce7e2d094ae467e15cf5c55ac643ea858
Willian Oliveira <wijloc@gmail.com>
2020-11-28 12:24:11 -0300

    feat: create repositories list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,42 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
+            alt="Willian Oliveira"
+          />
+          <div>
+            <strong>wijloc/gostack</strong>
+            <p>Short description</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
+            alt="Willian Oliveira"
+          />
+          <div>
+            <strong>wijloc/gostack</strong>
+            <p>Short description</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16838209?s=460&u=ee1a5b2999e614213112d9d7919d8603d3f9dcc2&v=4"
+            alt="Willian Oliveira"
+          />
+          <div>
+            <strong>wijloc/gostack</strong>
+            <p>Short description</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -38,10 +38,62 @@ export const Form = styled.form`
     border: 0;
     color: #fff;
     font-weight: bold;
-    transition: background-color 0.2;
+    transition: background 0.2s;
 
     &:hover {
       background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
